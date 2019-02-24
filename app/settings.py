c68745d9cafcfb5ebb81ed26613c1423e671cc79
Bohdan Sinchuk <sinchuk140995@gmail.com>
2019-02-25 01:04:14 +0200

    setup Celery and Redis; add redis and celery-worker images; postgres image: run as non-root user; python image: downgrade version to 3.6; nginx image: copy new nginx.conf without user nginx

--- app/settings.py
+++ app/settings.py
@@ -123,3 +123,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+# Celery
+
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+CELERY_BEAT_SCHEDULE = {}
